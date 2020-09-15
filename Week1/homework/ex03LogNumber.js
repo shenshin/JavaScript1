77d445272cf868779b5f0ab100d170f78647b80f
Ales Shenshin <shenshin@me.com>
2020-09-16 00:43:46 +0200

    Change variable name in ex03LogNumber.js from myString to numberX

--- Week1/homework/ex03LogNumber.js
+++ Week1/homework/ex03LogNumber.js
@@ -1,7 +1,7 @@
-'use strict'
-let myString
-console.log("The variable 'myString' is 'undefined'")
-console.log(myString)
-myString = 99
-console.log("The variable 'myString' is now initialized to the integer 99")
-console.log(myString)
+'use strict';
+let numberX;
+console.log("The variable 'numberX' is 'undefined'");
+console.log(numberX);
+numberX = 99;
+console.log("The variable 'numberX' is now initialized to the integer 99");
+console.log(numberX);
